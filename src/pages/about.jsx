5c859222cc66846171e0be58eb60813536624a64
chemistrysmash <chemistrysmash@gmail.com>
2024-07-04 16:52:44 +0800

    test for qr-scanner

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import service from "./../services";
-import QrScanner from 'react-qr-scanner';
+import QrReader from 'react-qr-scanner';
 import QrCodeScanner from '../components/QrCodeScanner';
 
 const About = () => {
   const [scanResult, setScanResult] = useState('');
 
-  const handleScanSuccess = (decodedText) => {
-    setScanResult(decodedText);
+  const handleScanSuccess = (decodedResult) => {
+    if (decodedResult && decodedResult.text) {
+      setScanResult(decodedResult.text);
+    }
   };
 
   const handleScanFailure = (errorMessage) => {
     console.warn(`QR Code scan failed: ${errorMessage}`);
   };
-
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  }
   return (
     <div>
       <h1>QR Code Scanner</h1>
-      <QrCodeScanner onScanSuccess={handleScanSuccess} onScanFailure={handleScanFailure} />
+      <QrReader
+          delay={100}
+          style={previewStyle}
+          onError={handleScanFailure}
+          onScan={handleScanSuccess}
+          facingMode={'rear'}
+          />
       <p>Scan Result: {scanResult}</p>
     </div>
   );
--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -2,12 +2,7 @@ import { useEffect, useState } from "react";
 import service from "./../services";
 
 function UserPage() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    service.user.getAll().then((allUsers) => {
-      setUsers(allUsers);
-    });
-  }, []);
+
   return (
     <div>
       <pre>{JSON.stringify(users, null, 2)}</pre>
